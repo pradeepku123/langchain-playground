aaee14dd12f5ae7b9dfe89f0802bc61b8a50ce63
Pradeep Kumar Behera <pradeepkumarstudent2011@gmail.com>
2024-10-25 02:04:05 +0000

    setup codespace

---
+++ app.py
@@ -0,0 +1,23 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# Load environment variables from a .env file
+load_dotenv()
+
+# Get the LANGCHAIN_API_KEY from the environment variables
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+print("LANGCHAIN_API_KEY Key is set to", os.environ["LANGCHAIN_API_KEY"])
+
+os.environ["GROQ_API_KEY"] =os.getenv("GROQ_API_KEY")
+print("GROQ_API_KEY Key is set to", os.environ["GROQ_API_KEY"])
+
+
+model = ChatGroq(model="llama3-8b-8192")
+
+print(model.invoke([HumanMessage(content="Who are you")]).content)
+
+from langchain_core.messages import AIMessage
